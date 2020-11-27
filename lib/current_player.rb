9a502ae90e827abb13b4fea103422865cf2a8120
Austin Hernandez <ahernandez1170@gmail.com>
2020-11-27 16:09:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def current_player(board)
+def turn_count(board)
   counter = 0
   board.each do |spaces|
     if spaces == "X" || spaces == "O"
